00fff35d0e8a73a9345b8fa9af205e881bf82f5e
Alexander Ivankov <AlexanderIvankoff@gmail.com>
2017-07-22 22:42:45 +0300

    Add gulp assembly react project and create first react components

--- frontend/assets/js/app.jsx
+++ frontend/assets/js/app.jsx
@@ -1,6 +1,9 @@
+// import * as ReactDOM from "react-dom";
+// import * as React from "react";
+
 console.log(React);
 console.log(ReactDOM);
-var News = React.createClass({
+let News = React.createClass({
 	render: function () {
 		return (
 				<div className="News">
@@ -10,12 +13,23 @@ var News = React.createClass({
 	}
 });
 
-var App = React.createClass({
+let Comments = React.createClass({
+	render: function () {
+		return (
+			<div className="comments">
+				Нет новостее - коментировать нечего
+			</div>
+		)
+	}
+});
+
+let App = React.createClass({
 	render: function () {
 		return (
 				<div className="App">
 					Всем привет, я компонент App и я умею возворащать новости!
 					<News/>
+					<Comments/>
 				</div>
 		)
 	}
--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,8 @@
 "use strict";
 
+// import * as ReactDOM from "react-dom";
+// import * as React from "react";
+
 console.log(React);
 console.log(ReactDOM);
 var News = React.createClass({
@@ -12,13 +15,24 @@ var News = React.createClass({
 	}
 });
 
+var Comments = React.createClass({
+	render: function render() {
+		return React.createElement(
+			"div",
+			{ className: "comments" },
+			"\u041D\u0435\u0442 \u043D\u043E\u0432\u043E\u0441\u0442\u0435\u0435 - \u043A\u043E\u043C\u0435\u043D\u0442\u0438\u0440\u043E\u0432\u0430\u0442\u044C \u043D\u0435\u0447\u0435\u0433\u043E"
+		);
+	}
+});
+
 var App = React.createClass({
 	render: function render() {
 		return React.createElement(
 			"div",
 			{ className: "App" },
 			"\u0412\u0441\u0435\u043C \u043F\u0440\u0438\u0432\u0435\u0442, \u044F \u043A\u043E\u043C\u043F\u043E\u043D\u0435\u043D\u0442 App \u0438 \u044F \u0443\u043C\u0435\u044E \u0432\u043E\u0437\u0432\u043E\u0440\u0430\u0449\u0430\u0442\u044C \u043D\u043E\u0432\u043E\u0441\u0442\u0438!",
-			React.createElement(News, null)
+			React.createElement(News, null),
+			React.createElement(Comments, null)
 		);
 	}
 });
